861eb2dde233831cffcde992e2eaca65a0d268ca
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-04-08 05:13:01 +0600

    Home Route Setup

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,5 +1,12 @@
+import { Outlet } from "react-router-dom";
+
 function Root() {
-  return <div>Root</div>;
+  return (
+    <>
+      <div>Root</div>
+      <Outlet></Outlet>
+    </>
+  );
 }
 
 export default Root;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,12 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../layout/Root";
 import Error from "../pages/error/Error";
+import Home from "../pages/home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
     errorElement: <Error></Error>,
+    children: [
+      {
+        index: true,
+        element: <Home></Home>,
+      },
+    ],
   },
 ]);
 
