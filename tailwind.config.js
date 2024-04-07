4b2137e6aadda974eec8fcac7b0f1a99d1248591
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-04-08 04:58:43 +0600

    website title, font family, favicon added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+      lato: ["Lato", "sans-serif"],
+      nunito: ["Nunito Sans", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
