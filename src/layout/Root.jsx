422c297965adcb667a54fda70cff7fbcc950501d
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-04-08 10:52:16 +0600

    Footer just Added

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../components/footer/Footer";
 import Navbar from "../components/header/Navbar";
 
 function Root() {
@@ -8,6 +9,7 @@ function Root() {
         <Navbar></Navbar>
       </header>
       <Outlet></Outlet>
+      <Footer></Footer>
     </>
   );
 }
