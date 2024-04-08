2093d27eaeac11ff29444e4f8c7395443b92b887
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-04-08 05:28:57 -0600

    Register And Login Page Added

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../layout/Root";
 import Error from "../pages/error/Error";
 import Home from "../pages/home/Home";
+import Login from "../pages/login/Login";
+import Register from "../pages/register/Register";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +14,14 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
       },
     ],
   },
