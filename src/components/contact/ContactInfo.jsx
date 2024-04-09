71a12379d3aaaa9fe17f63c75086aab2c7860256
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-04-09 05:13:28 -0600

    conact info updated

---
+++ src/components/contact/ContactInfo.jsx
@@ -0,0 +1,35 @@
+import { ImLocation } from "react-icons/im";
+import { MdLocalPhone } from "react-icons/md";
+import { SiAmazonsimpleemailservice } from "react-icons/si";
+import ContactInfoCart from "./ContactInfoCart";
+
+function ContactInfo() {
+  return (
+    <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-between gap-6 md:gap-8">
+      <ContactInfoCart
+        contactIcon={
+          <SiAmazonsimpleemailservice className="w-[120px] h-[120px]"></SiAmazonsimpleemailservice>
+        }
+        contactProvider={"  Email Address"}
+        contactAddress1={"dreammehedihassan@gmail.com"}
+        contactAddress2={"designermehedimb@gmail.com"}
+      ></ContactInfoCart>
+      <ContactInfoCart
+        contactIcon={
+          <MdLocalPhone className="w-[120px] h-[120px]"></MdLocalPhone>
+        }
+        contactProvider={"  Phone Number"}
+        contactAddress1={"(+880) 1716059182"}
+        contactAddress2={"(+880) 1783238465"}
+      ></ContactInfoCart>
+      <ContactInfoCart
+        contactIcon={<ImLocation className="w-[120px] h-[120px]"></ImLocation>}
+        contactProvider={"  Location"}
+        contactAddress1={"Mirzapur, Tangail"}
+        contactAddress2={"Dhaka, Bangladesh"}
+      ></ContactInfoCart>
+    </div>
+  );
+}
+
+export default ContactInfo;
