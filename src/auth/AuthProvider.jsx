2072b74dbbeda95f423effe7c284da52069953d2
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-04-08 21:41:00 -0600

    Registation Complete

--- src/auth/AuthProvider.jsx
+++ src/auth/AuthProvider.jsx
@@ -1,16 +1,43 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
 import PropTypes from "prop-types";
 import { createContext } from "react";
 import auth from "../firebase/firebase.init";
 export const AuthContext = createContext();
 
 function AuthProvider({ children }) {
-  // handle create user
+  // handle create user with email and password
   const handleRegister = (email, password) => {
-    createUserWithEmailAndPassword(auth, email, password);
+    return createUserWithEmailAndPassword(auth, email, password);
   };
+  //   handle create user with google account
+  const googleProvider = new GoogleAuthProvider();
+  const handleGoogleLogin = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  //   handle create user with github account
+  const githubProvider = new GithubAuthProvider();
+  const handleGithubLogin = () => {
+    return signInWithPopup(auth, githubProvider);
+  };
+
+  //   handle create user with twitter account
+  //   const facebookProvider = new FacebookAuthProvider();
+  //   const handleFacebookLogin = () => {
+  //     return signInWithPopup(auth, facebookProvider);
+  //   };
+
   // create all information about the auth users
-  const authInfo = { handleRegister };
+  const authInfo = {
+    handleRegister,
+    handleGoogleLogin,
+    handleGithubLogin,
+  };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
