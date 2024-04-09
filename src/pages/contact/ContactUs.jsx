43c6db8fc60447b7bf33263a3e2c0ff103fbf998
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-04-09 07:40:01 -0600

    Contact Address Section Title Added

--- src/pages/contact/ContactUs.jsx
+++ src/pages/contact/ContactUs.jsx
@@ -1,10 +1,17 @@
 import ContactForm from "../../components/contact/ContactForm";
 import ContactInfo from "../../components/contact/ContactInfo";
+import SectionTitle from "../../shareComponents/SectionTitle";
 
 function ContactUs() {
   return (
     <>
       <section className="my-6 md:my-8 lg:my-10">
+        <SectionTitle
+          title="Contact Address"
+          description={
+            "A contact address description typically refers to providing information about where an individual or organization can be reached. It usually includes details such as the physical address, email address, phone number, and possibly other relevant contact information."
+          }
+        ></SectionTitle>
         <ContactInfo></ContactInfo>
       </section>
 
