94d704f9c607b454b56686b42b5710243e4e199d
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-04-10 23:14:00 +0600

    firebase deploy

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,13 +1,19 @@
-import { Outlet } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import ClickToTop from "../components/clicktop/ClickToTop";
 import Footer from "../components/footer/Footer";
 import Navbar from "../components/header/Navbar";
-
-import AOS from "aos";
-import "aos/dist/aos.css";
 AOS.init();
 function Root() {
+  // after location change page is scroll top
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
   return (
     <>
       <ToastContainer></ToastContainer>
